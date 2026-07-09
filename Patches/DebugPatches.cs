24dd4a8d73223e296ba07666b4d99353216048da
Cave <20140539+cxve@users.noreply.github.com>
2026-07-09 21:18:18 +0200

    feat: add patch for extended logging and debugging

---
+++ Patches/DebugPatches.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cxve.qap.Patches;
+
+[HarmonyPatch]
+internal class DebugPatches
+{
+    [HarmonyPatch(typeof(SkillMap), nameof(SkillMap.InitializeLocal))]
+    [HarmonyPrefix]
+    public static void Log() => Plugin.Logger.LogDebug("If you are seeing this while respecing: The respec transpiler patch did not work!");
+}
