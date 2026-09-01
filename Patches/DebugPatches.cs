f3bc999064d83410d1e504088023e81677acf180
cxve <20140539+cxve@users.noreply.github.com>
2026-09-01 02:11:00 +0200

    chore: remove redundant debug patches

--- Patches/DebugPatches.cs
+++ Patches/DebugPatches.cs
@@ -5,7 +5,4 @@ namespace cxve.qap.Patches;
 [HarmonyPatch]
 internal class DebugPatches
 {
-    [HarmonyPatch(typeof(SkillMap), nameof(SkillMap.InitializeLocal))]
-    [HarmonyPrefix]
-    public static void Log() => Plugin.Logger.LogDebug("If you are seeing this while respecing: The respec transpiler patch did not work!");
 }
